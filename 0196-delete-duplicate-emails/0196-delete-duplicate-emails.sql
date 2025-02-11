6a9fccc67786bd513677031362041672d119106e
Ho Xuan Bac <94979655+bacpew2108@users.noreply.github.com>
2025-02-11 23:55:40 +0700

    Time: 1318 ms (56.39%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,5 +1,6 @@
-DELETE p1 
-FROM Person p1
-JOIN Person p2 
-ON p1.email = p2.email 
-AND p1.id > p2.id;
+WITH cte AS (
+    SELECT id, 
+           ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) AS rn
+    FROM Person
+)
+DELETE FROM Person WHERE id IN (SELECT id FROM cte WHERE rn > 1);
