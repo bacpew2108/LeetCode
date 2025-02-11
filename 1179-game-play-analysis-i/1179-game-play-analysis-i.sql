a301a32501daec4d251c3d5a1f3cdae66f881919
Ho Xuan Bac <94979655+bacpew2108@users.noreply.github.com>
2025-02-12 00:42:51 +0700

    Time: 959 ms (42.13%), Space: 0B (100%) - LeetHub

--- 1179-game-play-analysis-i/1179-game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/1179-game-play-analysis-i.sql
@@ -1,7 +1,6 @@
-SELECT player_id, event_date AS first_login
-FROM (
-    SELECT player_id, event_date,
-           RANK() OVER (PARTITION BY player_id ORDER BY event_date ASC) AS rnk
-    FROM Activity
-) sub
-WHERE rnk = 1;
+SELECT A1.player_id, A1.event_date AS first_login
+FROM Activity A1
+LEFT JOIN Activity A2
+ON A1.player_id = A2.player_id 
+AND A1.event_date > A2.event_date
+WHERE A2.player_id IS NULL;
