dc5514cfd4514949e22bce4cfa25cb7ad8a0485a
Ho Xuan Bac <94979655+bacpew2108@users.noreply.github.com>
2025-02-11 17:51:46 +0700

    Time: 935 ms (14.12%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,4 +1,4 @@
-Select distinct A.email AS Email
-from Person A
-join Person B on A.email = B.Email
-where A.id <> B.id;
+SELECT email AS Email
+FROM Person
+GROUP BY email
+HAVING COUNT(email) > 1;
