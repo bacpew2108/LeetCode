cf3cfcfd5ac5fdca09619b3ebe417de94a82a994
Ho Xuan Bac <94979655+bacpew2108@users.noreply.github.com>
2025-02-22 15:33:23 +0700

    Time: 727 ms (19.93%), Space: 0B (100%) - LeetHub

--- 3711-first-letter-capitalization-ii/3711-first-letter-capitalization-ii.sql
+++ 3711-first-letter-capitalization-ii/3711-first-letter-capitalization-ii.sql
@@ -1,29 +1,29 @@
-WITH RECURSIVE 
-    sub(id, text, length) AS (
-    select content_id, content_text, length(content_text)
-    from user_content
+with recursive 
+    sub(id, text, length) as (
+        select content_id, content_text, length(content_text) 
+        from user_content
     ),
-    table_char as (
-        select id,1 as text_index, substring(text,1,1) as c, text, length
+    char_table as (
+        select id, text,length , 1 as text_index, substring(text, 1, 1) as c
         from sub
-        UNION ALL
-        select id, text_index + 1, substring(text,text_index + 1, 1), text, length
-        from table_char
+        union all
+        select id, text,length, text_index + 1, substring(text, text_index + 1, 1) as c
+        from char_table
         where text_index + 1 <= length
     ),
     sub2 as (
-        select id, text_index, c, text, length, lag(c, 1, ' ') over (partition by id order by text_index) as prv_c
-        from table_char
+        select id, text_index, text, c, lag(c, 1, ' ') over (PARTITION BY id order by text_index) as prv_c
+        from char_table
     ),
     sub3 as (
-        select id, text, text_index, prv_c, c,
-        case 
-            when (prv_c = ' ' or prv_c = '-') and (c >='a' and c <= 'z') then upper(c)
-            when (prv_c >= 'a' and prv_c <= 'z') and (c >='a' and c <= 'z') then lower(c)
+        select id, text_index, text, prv_c, c, 
+        case
+            when (prv_c = ' ' or prv_c = '-') and (c between 'a' and 'z') then upper(c)
+            when (prv_c between 'A' and 'Z') and (c between 'a' and 'z') then lower(c)
             else c
-        end as next_c 
+        end as next_char
         from sub2
     )
-select id as content_id, text as original_text, group_concat(next_c order by text_index SEPARATOR '') as converted_text
+select id as content_id, text as original_text, group_concat(next_char order by text_index separator '') as converted_text
 from sub3
 group by id;
