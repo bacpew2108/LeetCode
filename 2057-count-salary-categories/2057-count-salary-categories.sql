b26b4c5da4a5eee87194a4c4a853f7727ead5c3b
Ho Xuan Bac <94979655+bacpew2108@users.noreply.github.com>
2025-02-26 12:28:48 +0700

    Time: 4214 ms (6.63%), Space: 0B (100%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.sql
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -1,19 +1,15 @@
-WITH salary_categories AS (
-    SELECT 'Low Salary' AS category
-    UNION ALL
-    SELECT 'Average Salary'
-    UNION ALL
-    SELECT 'High Salary'
-)
-select sc.category, count(type.category) as accounts_count
-from salary_categories as sc
-left join 
-(select
-case
-    when income < 20000 then "Low Salary"
-    when income between 20000 and 50000 then "Average Salary"
-    else "High Salary"
-end as category
-from Accounts) as type
-on sc.category = type.category
-group by sc.category;
+SELECT "Low Salary" AS category,
+       sum(income < 20000) AS accounts_count
+  FROM Accounts
+
+UNION
+
+SELECT "Average Salary" AS category,
+       sum(income BETWEEN 20000 AND 50000) AS accounts_count
+  FROM Accounts
+
+UNION
+
+SELECT "High Salary" AS category,
+       sum(income > 50000) AS accounts_count
+  FROM Accounts;
