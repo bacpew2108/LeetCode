419d2bb911acc4bf9b4eb272cc14caf289686093
Ho Xuan Bac <94979655+bacpew2108@users.noreply.github.com>
2025-04-24 17:05:00 +0700

    Time: 3 ms (99.19%), Space: 18.1 MB (30.9%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,23 +1,25 @@
-class Solution:
-    def convert(self, s: str, numRows: int) -> str:
-        n = numRows
-        result = []
-        if n < 2:
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1 or len(s) <= numRows:
             return s
-        for i in range(n):
-            step = (n-1)*2
-            if (i == 0 or i == n-1):
-                result.append(s[i::step])
-            else:
-                j = i
-                nextStep = (n-i-1)*2
-                c = 1
-                while j < len(s):
-                    result.append(s[j])
-                    if c == 1: 
-                        j = j + nextStep
-                    elif c == 0:
-                        j = j + step - nextStep
-                    c = 1 - c
-        result = ''.join(result)
-        return result
+            
+        rows = [''] * numRows
+        current_row = 0
+        step = 1
+        
+        for char in s:
+            rows[current_row] += char
+            
+            if current_row == 0:
+                step = 1
+            elif current_row == numRows - 1:
+                step = -1
+                
+            current_row += step
+            
+        return ''.join(rows)
